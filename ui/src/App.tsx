280a1a10cb878060c1969cd9392d17c995b5760f
Valerie Kraucunas <valeriekraucunas@gmail.com>
2020-01-26 19:07:00 -0700

    App WIP and styles

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import HawkTable from './components/HawkTable';
+import AddHawkForm from './components/AddHawkForm';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <header>
+        <h1>Hawk Collection</h1>
+        <button>Add Hawk</button>
+      </header>
+      <main>
+        <HawkTable />
+        <AddHawkForm />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
