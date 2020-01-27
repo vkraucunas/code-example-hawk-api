b530352fcb1e4fc06b7d33b5b00634cc3a618e51
Valerie Kraucunas <valeriekraucunas@gmail.com>
2020-01-26 19:06:32 -0700

    Hawk type

---
+++ ui/src/types/index.ts
@@ -0,0 +1,16 @@
+export interface Hawk {
+  id: number;
+  name: string;
+  gender: string;
+  size: string;
+  wingspanBegin?: string;
+  wingspanEnd?: string;
+  weightBegin?: string;
+  weightEnd?: string;
+  lengthBegin?: string;
+  lengthEnd?: string;
+  colorDescription?: string;
+  behaviorDescription?: string;
+  habitatDescription?: string;
+  pictureUrl?: string;
+}
